a14dce6f945f6d71ccae04f629728c9dcb5f1bcb
gs2-ci <ci@gs2.io>
2022-06-07 13:03:26 +0000

    97cf877152a85363a6a0f332e584ad88ec076a5d

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='gs2-python-sdk',
-    version='1.0.58',
+    version='1.0.59',
     package_dir={
         'gs2': '.'
     },
