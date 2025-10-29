8baeab4a8c61cc94f72a1aee3cb464260c931d8c
gs2-ci <ci@gs2.io>
2025-10-29 16:23:04 +0000

    792d7e689095d828add5567d9bb9e2fa14b10cd9

--- gs2/core/util.py
+++ gs2/core/util.py
@@ -14,7 +14,12 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from typing import Any
+from __future__ import annotations
+
+import os
+import sys
+import threading
+from typing import Any, Optional, Callable
 
 
 class timeout:
@@ -23,18 +28,64 @@ class timeout:
             seconds: int = 1,
             error_message: str = 'Timeout'
     ):
-        self.seconds = seconds
+        self.seconds = int(seconds)
         self.error_message = error_message
 
+        self._use_signal = False
+
+        self._timer: Optional[threading.Timer] = None
+        self._timed_out = False
+        self._prev_trace: Optional[Callable] = None
+
+    def _raise_timeout(self, *_: Any) -> None:
+        from multiprocessing import TimeoutError
+        raise TimeoutError(self.error_message)
     def handle_timeout(self, _: Any, __: Any):
         from multiprocessing import TimeoutError
         raise TimeoutError(self.error_message)
 
+    def _tracer(self, frame, event, arg):
+        if self._timed_out:
+            from multiprocessing import TimeoutError
+            raise TimeoutError(self.error_message)
+        return self._tracer
+
     def __enter__(self) -> None:
-        import signal
-        signal.signal(signal.SIGALRM, self.handle_timeout)
-        signal.alarm(self.seconds)
+        try:
+            import signal
+            if hasattr(signal, "SIGALRM") and os.name != "nt" and threading.current_thread() is threading.main_thread():
+                self._use_signal = True
+                signal.signal(signal.SIGALRM, self._raise_timeout)
+                signal.alarm(self.seconds)
+                return
+        except Exception:
+            self._use_signal = False
+
+        self._use_signal = False
+        self._timed_out = False
+        self._prev_trace = sys.gettrace()
+        sys.settrace(self._tracer)
+        self._timer = threading.Timer(self.seconds, self._set_timed_out)
+        self._timer.daemon = True
+        self._timer.start()
+
+    def _set_timed_out(self) -> None:
+        self._timed_out = True
 
     def __exit__(self, _type, value, traceback) -> None:
-        import signal
-        signal.alarm(0)
+        if self._use_signal:
+            import signal
+            try:
+                signal.alarm(0)
+            finally:
+                self._use_signal = False
+            return
+
+        try:
+            if self._timer is not None:
+                self._timer.cancel()
+        finally:
+            self._timer = None
+            self._timed_out = False
+            sys.settrace(self._prev_trace)
+            self._prev_trace = None
